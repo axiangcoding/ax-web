c5984fb7fcaad8c017307d9e2dce7e1314937824
axiangcoding <axiangcoding@gmail.com>
2022-08-24 13:16:32 +0800

    feat: support switch database type from config

--- settings/settings.go
+++ settings/settings.go
@@ -10,7 +10,6 @@ import (
 var Config AllConfig
 
 func Setup() {
-	setDefault()
 	viper.SetConfigName("app.toml")
 	viper.SetConfigType("toml")
 	viper.AddConfigPath("config/")
@@ -21,18 +20,11 @@ func Setup() {
 	viper.SetEnvKeyReplacer(replacer)
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Println("Config file not found. The program will use default conf and may not work properly")
+			log.Fatal("Config file not found. The application will not work properly")
 		}
 	}
 	err := viper.Unmarshal(&Config)
 	if err != nil {
 		log.Fatalf("Config Properties unable to decode into struct, %v", err)
 	}
-	// marshal, _ := json.MarshalIndent(Config, "-- ", "  ")
-	// log.Println("Effective configuration: " + string(marshal))
-}
-
-func setDefault() {
-	// generate a default config file maybe?
-	// 是否要生成一个默认的配置文件？
 }
